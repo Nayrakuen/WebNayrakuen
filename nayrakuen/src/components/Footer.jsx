1793fa51b89669b8ede3548577db6f4b30090c22
Project-STS <wahyuganss33@gmail.com>
2025-07-19 12:48:26 +0700

    Update Footer

--- nayrakuen/src/components/Footer.jsx
+++ nayrakuen/src/components/Footer.jsx
@@ -22,35 +22,35 @@ function Footer() {
               <h5>Halaman</h5>
               <ul>
                 <li><a href="/">Beranda</a></li>
-                <li><a href="/tentang">Tentang Kami</a></li>
-                <li><a href="/kontak">Hubungi Kami</a></li>
+                <li><a href="/tentang-kami">Tentang Kami</a></li>
+                <li><a href="mailto:nayrakuen@gmail.com">Hubungi Kami</a></li>
               </ul>
             </div>
 
             <div className="footer-group">
               <h5>Sosial Media</h5>
               <ul>
-                <li><a href="https://x.com/Nayrakuen_ID">X / Twitter</a></li>
-                <li><a href="https://www.instagram.com/nayrakuen_id/">Instagram</a></li>
-                <li><a href="https://www.tiktok.com/@nayrakuen_id">Tiktok</a></li>
+                <li><a href="https://x.com/Nayrakuen_ID" target="_blank" rel="noopener noreferrer">X / Twitter</a></li>
+                <li><a href="https://www.instagram.com/nayrakuen_id/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                <li><a href="https://www.tiktok.com/@nayrakuen_id" target="_blank" rel="noopener noreferrer">Tiktok</a></li>
               </ul>
             </div>
 
             <div className="footer-group">
               <h5>Sosial Media Nayla</h5>
               <ul>
-                <li><a href="https://x.com/SNayla_JKT48">X / Twitter</a></li>
-                <li><a href="https://www.instagram.com/jkt48.nayla.s/">Instagram</a></li>
-                <li><a href="https://www.tiktok.com/@jkt48.nayla">Tiktok</a></li>
-                <li><a href="https://www.threads.net/@jkt48.nayla.s">Threads</a></li>
-                <li><a href="https://www.idn.app/jkt48_nayla">IDN app</a></li>
-                <li><a href="https://www.showroom-live.com/r/JKT48_Nayla">Showroom</a></li>
+                <li><a href="https://x.com/SNayla_JKT48" target="_blank" rel="noopener noreferrer">X / Twitter</a></li>
+                <li><a href="https://www.instagram.com/jkt48.nayla.s/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                <li><a href="https://www.tiktok.com/@jkt48.nayla" target="_blank" rel="noopener noreferrer">Tiktok</a></li>
+                <li><a href="https://www.threads.net/@jkt48.nayla.s" target="_blank" rel="noopener noreferrer">Threads</a></li>
+                <li><a href="https://www.idn.app/jkt48_nayla" target="_blank" rel="noopener noreferrer">IDN App</a></li>
+                <li><a href="https://www.showroom-live.com/r/JKT48_Nayla" target="_blank" rel="noopener noreferrer">Showroom</a></li>
               </ul>
             </div>
 
             <div className="footer-group">
               <h5>Hashtag</h5>
-              <ul>
+              <ul className="hashtag-list">
                 <li>#Ohayo</li>
                 <li>#Naylabu</li>
                 <li>#NayReply</li>
